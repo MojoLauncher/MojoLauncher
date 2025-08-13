0283af77df8277b0f155979989dcc9ba2392ab0f
RaydanOMGr <andreykelbler@outlook.de>
2025-08-13 19:51:36 +0200

    styling stuff

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExperimentalFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceExperimentalFragment.java
@@ -2,6 +2,11 @@ package net.kdt.pojavlaunch.prefs.screens;
 
 import android.os.Bundle;
 
+import androidx.preference.ListPreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import git.artdeell.mojo.R;
 
 public class LauncherPreferenceExperimentalFragment extends LauncherPreferenceFragment {
@@ -9,5 +14,20 @@ public class LauncherPreferenceExperimentalFragment extends LauncherPreferenceFr
     @Override
     public void onCreatePreferences(Bundle b, String str) {
         addPreferencesFromResource(R.xml.pref_experimental);
+
+        ListPreference themesPref = requirePreference("select_theme", ListPreference.class);
+        List<String> newEntries = new ArrayList<>();
+        newEntries.add("Default");
+        newEntries.add("Midnight");
+        themesPref.setEntries(newEntries.toArray(new String[0]));
+
+        themesPref.setOnPreferenceChangeListener((preference, newValue) -> {
+            if(newValue.equals("Default")) {
+                requireActivity().setTheme(R.style.AppTheme);
+            } else if(newValue.equals("Midnight")) {
+                requireActivity().setTheme(R.style.MidnightTheme);
+            }
+            return true;
+        });
     }
 }
