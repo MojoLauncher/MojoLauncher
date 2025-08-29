682af7a93c7837f75c2c3e2eff959c80ebdc859e
ItsWinterBush <154499206+ItsWinterBush26@users.noreply.github.com>
2025-08-29 12:55:03 +0000

    feat ng_gl4es in the launcher

---
+++ ng_gl4es/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.bzlzhh.ng_gl4es"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+        ndkVersion = "27.1.12297006"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        create("proguard") {
+            isMinifyEnabled = true
+            initWith(getByName("debug"))
+        }
+        create("fordebug") {
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("NG-GL4ES/CMakeLists.txt")
+            version = "3.22.1"
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
