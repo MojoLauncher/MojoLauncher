10b6774a18dbd74b7b4f9c33b421dc3c36cbbb8c
whitebelyash <66699673+whitebelyash@users.noreply.github.com>
2025-11-20 23:44:48 +0400

    Feat[mgplugin]: tweaks and code style fixups

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/plugins/MGPlugin.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/plugins/MGPlugin.java
@@ -11,17 +11,19 @@ public class MGPlugin {
 
     public static LibraryPlugin initialize(Context context){
         mgPlugin = LibraryPlugin.discoverPlugin(context, "com.fcl.plugin.mobileglues");
-        if(mgPlugin != null)
-            MG_PATH = mgPlugin.resolveAbsolutePath("libmobileglues.so");
+        if (mgPlugin != null) MG_PATH = mgPlugin.resolveAbsolutePath("libmobileglues.so");
         return mgPlugin;
     }
     public static void load(){
         if(mgPlugin == null) return;
-        if(!JREUtils.dlopen(mgPlugin.resolveAbsolutePath("libspirv-cross-c-shared.so"))){
-            Log.e("MG", "Failed dlopening SPVX!");
+        if (!JREUtils.dlopen(mgPlugin.resolveAbsolutePath("libspirv-cross-c-shared.so"))) {
+            Log.e("MGInit", "Failed to pre-dlopen SPVX library!");
         }
-        if(!JREUtils.dlopen(mgPlugin.resolveAbsolutePath("libshaderconv.so"))){
-            Log.e("MG", "Failed dlopening shaderconv!");
+        if (!JREUtils.dlopen(mgPlugin.resolveAbsolutePath("libshaderconv.so"))) {
+            Log.e("MGInit", "Failed to pre-dlopen shaderconv!");
+        }
+        if (!JREUtils.dlopen(mgPlugin.resolveAbsolutePath("libmobileglues.so"))) {
+            Log.e("MGInit", "Failed to pre-dlopen main MobileGlues library. Check plugin installation");
         }
     }
 }
