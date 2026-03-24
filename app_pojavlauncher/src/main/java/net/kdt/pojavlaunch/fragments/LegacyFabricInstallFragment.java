f6484577926733f40f4d50706455814bdf6e7dee
Yarpopcat08 <109970976+Yarpopcat08@users.noreply.github.com>
2026-03-24 23:35:23 +0300

    feat[mod_installer/fabric]: add Legacy Fabric installer

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/LegacyFabricInstallFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/fragments/LegacyFabricInstallFragment.java
@@ -1,11 +1,11 @@
 package net.kdt.pojavlaunch.fragments;
 
 import net.kdt.pojavlaunch.modloaders.FabriclikeUtils;
-import net.kdt.pojavlaunch.modloaders.ModloaderListenerProxy;
 
 public class LegacyFabricInstallFragment extends FabriclikeInstallFragment {
 
     public static final String TAG = "LegacyFabricInstallFragment";
-    private static ModloaderListenerProxy sTaskProxy;
-    public LegacyFabricInstallFragment() {super(FabriclikeUtils.LEGACY_FABRIC_UTILS, TAG);}
+    public LegacyFabricInstallFragment() {
+        super(FabriclikeUtils.LEGACY_FABRIC_UTILS, TAG);
+    }
 }
