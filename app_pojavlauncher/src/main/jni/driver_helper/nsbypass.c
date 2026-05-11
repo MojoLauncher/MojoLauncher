7853ccf6885ccfdef5c3967b60bab39045a59143
Maksim Belov <ultramaksim@gmail.com>
2026-05-12 01:10:10 +0400

    Feat[pojavexec]: make nsbypass loader more flexible and improve GL loading
    This commit breaks building due to changes in vulkan_loader.c

--- app_pojavlauncher/src/main/jni/driver_helper/nsbypass.c
+++ app_pojavlauncher/src/main/jni/driver_helper/nsbypass.c
@@ -2,6 +2,7 @@
 // Created by maks on 05.06.2023.
 //
 #include "nsbypass.h"
+#include "android_namespace_func.h"
 #include <dlfcn.h>
 #include <android/dlext.h>
 #include <android/log.h>
@@ -16,84 +17,28 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <elf.h>
+#include <elf_defs.h>
 #include <inttypes.h>
 
-/* upper 6 bits of an ARM64 instruction are the instruction name */
-#define OP_MS 0b11111100000000000000000000000000
-/* Branch Label instruction opcode and immediate mask */
-#define BL_OP 0b10010100000000000000000000000000
-#define BL_IM 0b00000011111111111111111111111111
+#define TAG __FILE_NAME__
+#include <log.h>
+
 /* Library search path */
 #define SEARCH_PATH "/system/lib64"
-#define ELF_EHDR Elf64_Ehdr
-#define ELF_SHDR Elf64_Shdr
-#define ELF_HALF Elf64_Half
-#define ELF_XWORD Elf64_Xword
-#define ELF_DYN Elf64_Dyn
 
-//#define ADRENO_POSSIBLE
-
-typedef void* (*loader_dlopen_t)(const char* filename, int flags, const void* caller_addr);
-
-typedef struct android_namespace_t* (*ld_android_create_namespace_t)(
-        const char* name, const char* ld_library_path, const char* default_library_path, uint64_t type,
-        const char* permitted_when_isolated_path, struct android_namespace_t* parent, const void* caller_addr);
-
-typedef void* (*ld_android_link_namespaces_t)(struct android_namespace_t* namespace_from,
-                                              struct android_namespace_t* namespace_to,
-                                              const char* shared_libs_sonames);
-
-static ld_android_create_namespace_t android_create_namespace;
 static struct android_namespace_t* driver_namespace = NULL;
 
-struct android_namespace_t* local_android_create_namespace(
-        const char* name, const char* ld_library_path, const char* default_library_path, uint64_t type,
-        const char* permitted_when_isolated_path, struct android_namespace_t* parent) {
-    void* caller = __builtin_return_address(0);
-    return android_create_namespace(name, ld_library_path, default_library_path, type, permitted_when_isolated_path, parent, caller);
-}
-
-// Find the first "branch to label" function in the function provided in func_start
-static void* find_branch_label(void* func_start) {
-    int pagesize = getpagesize();
-    // round down the pointer to get the start of the function's page
-    void* func_page_start = (void*)(((uintptr_t)func_start) & ~(pagesize-1));
-    // remap to r-x to bypass "execute only" protections on MIUI
-    mprotect(func_page_start, pagesize, PROT_READ | PROT_EXEC);
-    uint32_t* bl_addr = func_start;
-    // search for the "branch to label" opcode
-    while((*bl_addr & OP_MS) != BL_OP) {
-        bl_addr++; // walk through memory until we find it or die
-    }
-    // offset the address to find where the "branch to label" instrunction
-    // points to.
-    return ((char*)bl_addr) + (*bl_addr & BL_IM) * 4;
-}
-
 bool linker_ns_load(const char* lib_search_path) {
-#ifndef ADRENO_POSSIBLE
-    return false;
-#else
     if(driver_namespace != NULL) return true; // Do not initialize namespaces multiple times, this caused very funny bugs we spent hours debugging
-    int pagesize = getpagesize();
-    loader_dlopen_t loader_dlopen = find_branch_label(&dlopen);
-    // reprotecting the functions removes protection from indirect jumps
-    mprotect(loader_dlopen, pagesize, PROT_WRITE | PROT_READ | PROT_EXEC);
-    void* ld_android_handle = loader_dlopen("ld-android.so", RTLD_LAZY, &dlopen);
-    if(ld_android_handle == NULL) {
+    android_ldfuncs_t ldfuncs;
+    if(!locate_namespace_funcs(&ldfuncs)) {
         return false;
     }
-    // load the two functions we need
-    android_create_namespace = dlsym(ld_android_handle, "__loader_android_create_namespace");
-    ld_android_link_namespaces_t android_link_namespaces = dlsym(ld_android_handle, "__loader_android_link_namespaces");
-    if(android_create_namespace == NULL || android_link_namespaces == NULL) {
-        dlclose(ld_android_handle);
-        return false;
-    }
+
     // assemble the full path search path
     char full_path[strlen(SEARCH_PATH) + strlen(lib_search_path) + 2 + 1];
     sprintf(full_path, "%s:%s", SEARCH_PATH, lib_search_path);
-    driver_namespace = local_android_create_namespace("pojav-driver",
+    driver_namespace = ldfuncs.create_namespace("pojav-driver",
                                                       full_path,
                                                       full_path,
                                                       3 /* TYPE_SHAFED | TYPE_ISOLATED */,
@@ -106,27 +51,22 @@ bool linker_ns_load(const char* lib_search_path) {
     // a lot of android versions
     // FoldCraft got trolled because they copied the
     // old broken code verbatim and didn't even test it thoroughly
-    android_link_namespaces(driver_namespace, NULL, "ld-android.so");
+    ldfuncs.link_namespaces(driver_namespace, NULL, "ld-android.so");
     // Also establish links to use the libnativeloader(_lazy).so libraries
     // from the global namespace. This is a workaround for an EMUI issue where
     // the newly loaded libnativeloader_lazy for some unknown reason links
     // to itself and causes a deadlock when loading the vulkan driver.
-    android_link_namespaces(driver_namespace, NULL, "libnativeloader.so");
-    android_link_namespaces(driver_namespace, NULL, "libnativeloader_lazy.so");
-    dlclose(ld_android_handle);
+    ldfuncs.link_namespaces(driver_namespace, NULL, "libnativeloader.so");
+    ldfuncs.link_namespaces(driver_namespace, NULL, "libnativeloader_lazy.so");
+    ldfuncs.close(ldfuncs.dl_handle);
     return true;
-#endif
 }
 
 void* linker_ns_dlopen(const char* name, int flag) {
-#ifndef ADRENO_POSSIBLE
-    return NULL;
-#else
     android_dlextinfo dlextinfo;
     dlextinfo.flags = ANDROID_DLEXT_USE_NAMESPACE;
     dlextinfo.library_namespace = driver_namespace;
     return android_dlopen_ext(name, flag, &dlextinfo);
-#endif
 }
 
 bool patch_elf_soname(int patchfd, int realfd, size_t size, const char* patchname) {
@@ -167,9 +107,6 @@ bool patch_elf_soname(int patchfd, int realfd, size_t size, const char* patchnam
 #define PAGE_ALIGN(addr)        (((addr)+pagesize-1)&(~(pagesize-1)))
 
 void* linker_ns_dlopen_unique(const char* tmpdir, const char* name, const char* patch_name, int flags) {
-#ifndef ADRENO_POSSIBLE
-    return NULL;
-#else
     int pagesize = getpagesize();
     char pathbuf[PATH_MAX];
     static uint16_t patchid;
@@ -187,7 +124,7 @@ void* linker_ns_dlopen_unique(const char* tmpdir, const char* name, const char* 
         totalsize = PAGE_ALIGN(fsize);
     }
 
-    patch_fd = syscall(__NR_memfd_create, patch_name, MFD_CLOEXEC);
+    patch_fd = (int) syscall(__NR_memfd_create, patch_name, MFD_CLOEXEC);
     if(patch_fd == -1) {
         // TODO: use ASharedMemory as fallback
         // NOTE: use page-aligned size (totalsize) for ashmem
@@ -216,5 +153,4 @@ void* linker_ns_dlopen_unique(const char* tmpdir, const char* name, const char* 
     fail_real:
     close(real_fd);
     return NULL;
-#endif
 }
