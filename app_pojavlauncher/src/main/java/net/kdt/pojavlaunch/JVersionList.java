dde09d356652d370b16fdff65f3981428a84d72d
whitebelyash <whbexiumwork@gmail.com>
2026-09-06 18:31:39 +0400

    Revert "Feat[downloader]: support excluding libraries after inheritsFrom"
    This reverts commit 9d631a11958aba738b47723ad33926c38a770c73.

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/JVersionList.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/JVersionList.java
@@ -27,7 +27,6 @@ public class JVersionList {
         public String inheritsFrom;
         public JavaVersionInfo javaVersion;
         public DependentLibrary[] libraries;
-        public String[] excludedLibraries;
         public LoggingConfig logging;
         public String mainClass;
         public String minecraftArguments;
