ece6dc86822fc9a589f0739f3e76b07ca7b5e649
whitebelyash <whbexiumwork@gmail.com>
2026-09-17 01:40:51 +0400

    Chore[prefs]: clean unused imports

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceMiscellaneousFragment.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/prefs/screens/LauncherPreferenceMiscellaneousFragment.java
@@ -2,7 +2,6 @@ package net.kdt.pojavlaunch.prefs.screens;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,7 +18,6 @@ import net.kdt.pojavlaunch.PojavApplication;
 import net.kdt.pojavlaunch.Tools;
 import net.kdt.pojavlaunch.progresskeeper.ProgressKeeper;
 import net.kdt.pojavlaunch.tasks.DataMigrator;
-import net.kdt.pojavlaunch.utils.GpuUtils;
 
 import org.apache.commons.io.FileUtils;
 
